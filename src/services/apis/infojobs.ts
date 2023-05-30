22aeb15426dca171c7f5fadc1ef8b2ec1c809c41
Carlos Segura <lithos.contact@gmail.com>
2023-05-30 08:45:51 +0200

    improve error messages and add .env example

--- src/services/apis/infojobs.ts
+++ src/services/apis/infojobs.ts
@@ -4,7 +4,7 @@ export const getOffers = async (params: string) => {
 	try {
 		return await axios.get(`${import.meta.env.VITE_BASE_URL}${params}`);
 	} catch (error) {
-		console.log('ERROR: ', error);
+		console.log('Error getting offers list: ', error);
 	}
 };
 
@@ -12,6 +12,6 @@ export const getOffer = async (id: string) => {
 	try {
 		return await axios.get(`${import.meta.env.VITE_BASE_URL}/${id}`);
 	} catch (error) {
-		console.log('ERROR: ', error);
+		console.log('Error getting offer details: ', error);
 	}
 };
